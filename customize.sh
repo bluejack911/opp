796dd0adc3eeb1d6b2cccbf056a84347e634b7df
bluejack911 <49217273+bluejack911@users.noreply.github.com>
2022-11-22 21:19:24 +0800

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
+#=================================================
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.100.1/g' openwrt/package/base-files/files/bin/config_generate
+
+#2. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#3. Replace with JerryKuKu’s Argon
+#rm openwrt/package/lean/luci-theme-argon -rf
+
+#4 Add a feed source
+#echo 'src-git weifuture https://github.com/hongweifuture/openwrt-packages' >>feeds.conf.default
+#echo 'src-git pwdep https://github.com/hongweifuture/pwdep.git' >>feeds.conf.default
